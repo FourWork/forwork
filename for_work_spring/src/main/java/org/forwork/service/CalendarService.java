23f70bb156e3ee1db0c375d3d4053d8c41b3909d
ga0-lee <lleun8956@naver.com>
2021-05-26 16:27:51 +0900

    create : week calendar files

--- for_work_spring/src/main/java/org/forwork/service/CalendarService.java
+++ for_work_spring/src/main/java/org/forwork/service/CalendarService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.forwork.domain.Calendar;
 import org.forwork.dto.CalendarDto;
+import org.forwork.dto.CalendarWeekDto;
 
 public interface CalendarService {
 	public int registerCalendar(Calendar cal);
 	public List<CalendarDto> listCalendar(String project_id);
+	public List<CalendarWeekDto> listWeek(String project_id);
 	
 	public int updateCalendar(Calendar cal);
 }
