93a360433e65bd9f7c59bd38fa0a2ed1fa537c26
Seungyub <iiwvii@naver.com>
2021-05-27 10:53:56 +0900

    feat : project 별 scrum board socket 분류추가

--- for_work_spring/src/main/java/org/forwork/socket/TaskWebSocketServer.java
+++ for_work_spring/src/main/java/org/forwork/socket/TaskWebSocketServer.java
@@ -3,7 +3,9 @@ package org.forwork.socket;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.websocket.OnClose;
 import javax.websocket.OnError;
@@ -11,6 +13,7 @@ import javax.websocket.OnMessage;
 import javax.websocket.OnOpen;
 import javax.websocket.RemoteEndpoint.Basic;
 import javax.websocket.Session;
+import javax.websocket.server.PathParam;
 import javax.websocket.server.ServerEndpoint;
 
 import org.springframework.stereotype.Controller;
@@ -18,11 +21,10 @@ import org.springframework.stereotype.Controller;
 import lombok.extern.log4j.Log4j;
 
 @Controller
-@ServerEndpoint(value = "/taskSocket") // socket connection url
+@ServerEndpoint(value = "/taskSocket/{project_id}") // socket connection url
 @Log4j
 public class TaskWebSocketServer {
-	//추가 사항 : project별로 구분 필요 
-	private static final List<Session> sessionList = new ArrayList<>(); // 연결된 세션 모음
+	private static final Map<Integer, List<Session>> sessionMap = new HashMap<Integer, List<Session>>();
 	
 	public TaskWebSocketServer() { // websocket 생성
 		System.out.println("web socket create");
@@ -30,7 +32,7 @@ public class TaskWebSocketServer {
 	
 	
 	@OnOpen
-	public void onOpen(Session session){ 
+	public void onOpen(Session session, @PathParam("project_id")int project_id){ 
 		log.info("open session : " + session.getId());
 		try{ // 정상적으로 연결시 메세지 전달
 			final Basic basic = session.getBasicRemote();
@@ -38,12 +40,17 @@ public class TaskWebSocketServer {
 		}catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		sessionList.add(session);
+		if(sessionMap.containsKey(project_id)){
+			sessionMap.get(project_id).add(session);
+		}else{
+			sessionMap.put(project_id, new ArrayList<>());
+			sessionMap.get(project_id).add(session);
+		}
 	}
 	
-	private void sendAllSessionToMessage(Session self,String msg){ // 모든 session의 유저에게 메세지 전달 broadCast
+	private void sendAllSessionToMessage(Session self,String msg,int projcet_id){ // 모든 session의 유저에게 메세지 전달 broadCast
 		try {
-			for(Session s : TaskWebSocketServer.sessionList){
+			for(Session s : TaskWebSocketServer.sessionMap.get(projcet_id)){
 				if(!self.getId().equals(s.getId())){
 					s.getBasicRemote().sendText("c");
 				}
@@ -55,7 +62,7 @@ public class TaskWebSocketServer {
 	
 	
 	@OnMessage // 메세지 전달시
-	public void onMessage (String message, Session session){
+	public void onMessage (String message, Session session,@PathParam("project_id")int project_id){
 		try {
 			//메세지 보낸 사람에게 표시될 내용
 			final Basic basic = session.getBasicRemote();
@@ -64,16 +71,17 @@ public class TaskWebSocketServer {
 			System.out.println(e.getMessage());
 		}
 		// 다른 사람에게 메세지 보내기
-		sendAllSessionToMessage(session,message); // 전달 받은 메세지 broad cast
+		sendAllSessionToMessage(session,message,project_id); // 전달 받은 메세지 broad cast
 	}
 	
 	@OnError
 	public void onError(Throwable e, Session session){
 		log.info(e.getMessage() + "by session : " + session.getId());
 	}
+	
 	@OnClose
-	public void onClose(Session session){
+	public void onClose(Session session, @PathParam("project_id")int project_id){
 		log.info("Session : "+ session.getId() + " closed");
-		sessionList.remove(session); // 연결 종료 시 session 삭제
+		sessionMap.get(project_id).remove(session); // 연결 종료 시 session 삭제
 	}
 }
